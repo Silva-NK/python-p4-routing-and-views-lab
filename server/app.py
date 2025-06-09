45fb78053815e5f25098b0b8b6d6b747d841c68e
Silva-NK <kiagonsuzanne@gmail.com>
2025-06-10 00:40:21 +0300

    Adds decorator routing functions and views.

--- server/app.py
+++ server/app.py
@@ -6,3 +6,42 @@ app = Flask(__name__)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
+
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+
+@app.route('/print/<string:parameter>')
+def print_string(parameter):
+    print(parameter)
+
+    return f'{parameter}'
+
+
+@app.route('/count/<int:parameter>')
+def count(parameter):
+    numbers = [str(i) for i in range(parameter)]
+
+    return '\n'.join(numbers) + '\n'
+
+
+@app.route('/math/<int:num1>/<operation>/<int:num2>')
+def math(num1, operation, num2):
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == 'div':
+        if num2 == 0:
+            return 'Error: Division by zero is not allowed.'
+        result = num1 / num2
+    elif operation == '%':
+        result = num1 % num2
+    else:
+        return 'Invalid operation. Supported operations: +, -, *, div, %.'
+    
+    return str(result)
